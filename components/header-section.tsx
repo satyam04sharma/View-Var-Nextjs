377e28b1672c0007138601afe2d48db0ba9223fb
satyam04sharma <satyam04sharma>
2024-04-21 01:38:55 +0000

    Pushed from Locofy Builder

---
+++ components/header-section.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./header-section.module.css";
+
+const HeaderSection: NextPage = () => {
+  return <div className={styles.background} />;
+};
+
+export default HeaderSection;
---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./header.module.css";
+
+const Header: NextPage = () => {
+  return <div className={styles.background} />;
+};
+
+export default Header;
